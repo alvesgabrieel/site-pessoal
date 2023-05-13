f66e9c0622f5bb0776bba62729419cda678d44ef
Gabriel Alves <gabriel_alvesqm@hotmail.com>
2023-05-13 20:41:00 -0300

    update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,61 +1,100 @@
-import React from 'react';
+import React from "react";
 //imgs
-import Image from '../assets/banner.png';
+import Image from "../assets/banner.png";
 //icons
-import { FaGithub, FaLinkedin, FaInstagram } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 // type animation
-import { TypeAnimation } from 'react-type-animation';
+import { TypeAnimation } from "react-type-animation";
 //motion
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 //variants
-import { fadeIn } from '../variants';
+import { fadeIn } from "../variants";
 // link
-import { Link } from 'react-scroll';
+import { Link } from "react-scroll";
 //css banner
-import '../index.css';
- 
+import "../index.css";
+
 const Banner = () => {
   return (
-    <section id='home' className='min-h-[85vh] lg:min-h-[78vh] flex items-center'>
+    <section
+      id="home"
+      className="min-h-[85vh] lg:min-h-[78vh] flex items-center"
+    >
       <div className="container mx-auto">
-        <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12'>
-          <div className='flex-1 text-center font-secondary lg:text-left'>
-            <motion.h1 variants={fadeIn('up', 0.3)} initial='hidden' whileInView={'show'} viewport={{ onde: false, amount: 0.7 }} className='text-[55px] font-bold leading-[0.8] lg:text-[110px]'>
+        <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12">
+          <div className="flex-1 text-center font-secondary lg:text-left">
+            <motion.h1
+              variants={fadeIn("up", 0.3)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ onde: false, amount: 0.7 }}
+              className="text-[55px] font-bold leading-[0.8] lg:text-[110px]"
+            >
               GABRIEL <span>ALVES</span>
             </motion.h1>
-            <motion.div variants={fadeIn('up', 0.4)} initial='hidden' whileInView={'show'} viewport={{ onde: false, amount: 0.7 }} className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leadin-[1]'>
-              <span className='text-white mr-1'>desenvolvedor</span>
-              <TypeAnimation sequence={[
-                'Web',
-                2000,
-                'React',
-                2000,
-                'JavaScript',
-                2000,
-              ]} 
-              speed={50}
-              className='text-accent'
-              wrapper='span'
-              repeat={Infinity}
+            <motion.div
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ onde: false, amount: 0.7 }}
+              className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leadin-[1]"
+            >
+              <span className="text-white mr-1">desenvolvedor</span>
+              <TypeAnimation
+                sequence={["Web", 2000, "React", 2000, "JavaScript", 2000]}
+                speed={50}
+                className="text-accent"
+                wrapper="span"
+                repeat={Infinity}
               />
             </motion.div>
-            <motion.p variants={fadeIn('up', 0.5)} initial='hidden' whileInView={'show'} viewport={{ onde: false, amount: 0.7 }} className='mb-8 max-wid-lg mx-auto lg:mx-0'>
-            Atualmente meu foco é no Desenvolvimento Web Front-end, especificamente direcionado ao desenvolvimento de interfaces SPA (Single Page Application) responsivas (desktop, tablet, mobile), tendo como meta tornar-me Fullstack. 
+            <motion.p
+              variants={fadeIn("up", 0.5)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ onde: false, amount: 0.7 }}
+              className="mb-8 max-wid-lg mx-auto lg:mx-0"
+            >
+              Atualmente meu foco é no Desenvolvimento Web Front-end,
+              desenvolvendo aplicações web responsivas com as tecnologias mais
+              atualizadas do mercado, tendo como meta tornar-me Fullstack.
             </motion.p>
-            <motion.div variants={fadeIn('up', 0.7)} initial='hidden' whileInView={'show'} viewport={{ onde: false, amount: 0.7 }} className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
-              <Link to='contact' activeClass='active' smooth={true} spy={true}>
-                <button className='btn btn-lg'>Contate-me</button>
+            <motion.div
+              variants={fadeIn("up", 0.7)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ onde: false, amount: 0.7 }}
+              className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0"
+            >
+              <Link to="contact" activeClass="active" smooth={true} spy={true}>
+                <button className="btn btn-lg">Contate-me</button>
               </Link>
-              <Link to='work' activeClass='active' smooth={true} spy={true}>
-                <a href="#" className='text-gradient btn-link ml-2'>Projetos</a>
-              </Link>              
+              <Link to="work" activeClass="active" smooth={true} spy={true}>
+                <a href="#" className="text-gradient btn-link ml-2">
+                  Projetos
+                </a>
+              </Link>
             </motion.div>
-            { /* socials */}
-            <motion.div variants={fadeIn('up', 0.7)} initial='hidden' whileInView={'show'} viewport={{ onde: false, amount: 0.7 }} className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0'>
-              <a href="https://github.com/alvesgabrieel" target="_blank">
+            {/* socials */}
+            <motion.div
+              variants={fadeIn("up", 0.7)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ onde: false, amount: 0.7 }}
+              className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0"
+            >
+              <a
+                href="https://github.com/alvesgabrieel"
+                target="_blank"
+                rel="noreferrer"
+              >
                 <FaGithub />
               </a>
-              <a href="https://www.linkedin.com/in/gabriel-alvesqm-desenvolvedor/" target="_blank">
+              <a
+                href="https://www.linkedin.com/in/gabriel-alvesqm-desenvolvedor/"
+                target="_blank"
+                rel="noreferrer"
+              >
                 <FaLinkedin />
               </a>
               <a href="#">
@@ -64,8 +103,17 @@ const Banner = () => {
             </motion.div>
           </div>
           {/* image */}
-          <motion.div variants={fadeIn('down', 0.5)} initial='hidden' whileInView={'show'} className='text-center'>
-            <img className='App-logo block mx-auto' src={Image} alt="Image Profile" />
+          <motion.div
+            variants={fadeIn("down", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            className="text-center"
+          >
+            <img
+              className="App-logo block mx-auto"
+              src={Image}
+              alt="profile imag"
+            />
           </motion.div>
         </div>
       </div>
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,102 +1,145 @@
 // COMPONENT NEED REVIEW (mobile)
-import React from 'react';
+import React from "react";
 // motion
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 // variants
-import { fadeIn } from '../variants';
+import { fadeIn } from "../variants";
 // img
-import Img1 from '../assets/portfolio-img1.png';
-import Img2 from '../assets/portfolio-img2.png';
-import Img3 from '../assets/portfolio-img3.png';
+import Img1 from "../assets/portfolio-img1.png";
+import Img2 from "../assets/portfolio-img2.png";
+import Img3 from "../assets/portfolio-img3.png";
 
 const Work = () => {
   //open link w/ button
   const link = () => {
-    return window.open("https://github.com/alvesgabrieel", "_blank")
-  }
+    return window.open("https://github.com/alvesgabrieel", "_blank");
+  };
 
   return (
-    <section className='section' id='work' >
-
+    <section className="section" id="work">
       <div className="container mx-auto">
-
-        <div className='flex flex-col lg:flex-row gap-x-10'>
-
-          <motion.div className='flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0' variants={fadeIn('right', 0.3)} initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.3 }} >
-            
+        <div className="flex flex-col lg:flex-row gap-x-10">
+          <motion.div
+            className="flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0"
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+          >
             {/* text */}
             <div>
-              <h2 className='h2 leading-tight text-accent'>
-                Alguns<br />
+              <h2 className="h2 leading-tight text-accent">
+                Alguns
+                <br />
                 Projetos
               </h2>
-              <p className='max-w-sm mb-16'>
-                Alguns projetos feitos por mim e um pouco sobre eles, para ver todos os projetos é só acessar meu Github, clicando no botão abaixo:
-              </p>              
+              <p className="max-w-sm mb-16">
+                Alguns projetos feitos por mim e um pouco sobre eles, para ver
+                todos os projetos é só acessar meu Github, clicando no botão
+                abaixo:
+              </p>
 
-                <button className='btn btn-sm' onClick={link}>
+              <button className="btn btn-sm" onClick={link}>
                 Ver todos os projetos no Github
-                </button>                           
+              </button>
             </div>
 
             {/* image */}
-            <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+            <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
               {/* overlay */}
-              <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
               {/* img */}
-              <img className='group-hover:scale-125 transition-all duration-500' src={Img1} alt="projeto clone pagina vingadores" />
+              <img
+                className="group-hover:scale-125 transition-all duration-500"
+                src={Img1}
+                alt="projeto clone pagina vingadores"
+              />
               {/* pretitle */}
-              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
-                <span className='text-gradient'>
-                  <a href="https://disney-plus-pagina-vingadores.vercel.app/" target="_blank">Clone Vingadores - Ultimato</a> 
+              <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                <span className="text-gradient">
+                  <a
+                    href="https://disney-plus-pagina-vingadores.vercel.app/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Clone Vingadores - Ultimato
+                  </a>
                 </span>
               </div>
               {/* title */}
-              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
-                <span className='text-3x1 text-white'>w/ HTML, SASS, JS PURO</span>                
-              </div>
-            </div> 
-
-          </motion.div>
-
-          <motion.div className='flex-1 flex flex-col gap-y-10 mt-[-14px]' variants={fadeIn('left', 0.2)} initial='hidden' whileInView={'show'} viewport={{ once: false, amount: 0.3 }}>
-            {/* image */}
-            <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
-              {/* overlay */}
-              <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
-              {/* img */}
-              <img className='group-hover:scale-125 transition-all duration-500' src={Img2} alt="" />
-              {/* pretitle */}
-              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
-                <a href="https://clone-disneyplus-streaming-1rm80gpux-alvesgabrieel.vercel.app/" target="_blank"><span className='text-gradient'>Clone Landing page Streaming Disney+</span></a>                
-              </div>
-              {/* title */}
-              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
-                <span className='text-3x1 text-white'>w/ HTML, SASS, JS PURO</span>                
-              </div>
-            </div>
-
-            {/* image */}
-            <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
-              {/* overlay */}
-              <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
-              {/* img */}
-              <img className='group-hover:scale-125 transition-all duration-500' src={Img3} alt="" />
-              {/* pretitle */}
-              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
-                <a href="https://landing-page-evento.vercel.app/" target="_blank"><span className='text-gradient'>Landing Page Evento de tecnologia</span></a>                
-              </div>
-              {/* title */}
-              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
-                <span className='text-3x1 text-white'>w/ HTML, SASS, JS PURO</span>                
+              <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                <span className="text-3x1 text-white">
+                  w/ HTML, SASS, JS PURO
+                </span>
               </div>
             </div>
           </motion.div>
 
+          <motion.div
+            className="flex-1 flex flex-col gap-y-10 mt-[-14px]"
+            variants={fadeIn("left", 0.2)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+          >
+            {/* image */}
+            <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+              {/* overlay */}
+              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+              {/* img */}
+              <img
+                className="group-hover:scale-125 transition-all duration-500"
+                src={Img2}
+                alt=""
+              />
+              {/* pretitle */}
+              <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                <a
+                  href="https://clone-disneyplus-streaming-1rm80gpux-alvesgabrieel.vercel.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <span className="text-gradient">
+                    Clone Landing page Streaming Disney+
+                  </span>
+                </a>
+              </div>
+              {/* title */}
+              <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                <span className="text-3x1 text-white">
+                  w/ HTML, SASS, JS PURO
+                </span>
+              </div>
+            </div>
+
+            {/* image */}
+            <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+              {/* overlay */}
+              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+              {/* img */}
+              <img
+                className="group-hover:scale-125 transition-all duration-500"
+                src={Img3}
+                alt=""
+              />
+              {/* pretitle */}
+              <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                <a
+                  href="https://weekaway-gaqm.vercel.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <span className="text-gradient">Website WEEKAWAY</span>
+                </a>
+              </div>
+              {/* title */}
+              <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                <span className="text-3x1 text-white">w/ REACT, TAILWIND</span>
+              </div>
+            </div>
+          </motion.div>
         </div>
-
       </div>
-
     </section>
   );
 };
